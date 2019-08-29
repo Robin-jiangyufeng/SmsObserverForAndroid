a73ba1a92df0619ffeadbcaf9d4fb5c583609f0d
jiangyufeng <429257411@qq.com>
2019-08-29 16:07:17 +0800

    1.fix

--- Sample/src/main/java/com/robin/lazy/sample/App.java
+++ Sample/src/main/java/com/robin/lazy/sample/App.java
@@ -13,8 +13,6 @@ package com.robin.lazy.sample;
 
 import android.app.Application;
 
-import com.karumi.dexter.Dexter;
-
 /**
  * <一句话功能简述>
  *
@@ -27,6 +25,5 @@ public class App extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-        Dexter.initialize(this);
     }
 }
--- Sample/src/main/java/com/robin/lazy/sample/MainActivity.java
+++ Sample/src/main/java/com/robin/lazy/sample/MainActivity.java
@@ -6,7 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.karumi.dexter.Dexter;
-import com.karumi.dexter.listener.single.CompositePermissionListener;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.robin.lazy.sms.SmsObserver;
 import com.robin.lazy.sms.SmsResponseCallback;
 import com.robin.lazy.sms.VerificationCodeSmsFilter;
@@ -22,7 +26,13 @@ public class MainActivity extends AppCompatActivity implements SmsResponseCallba
         smsObserver=new SmsObserver(this,this,new VerificationCodeSmsFilter("180"));
         smsObserver.registerSMSObserver();
         textView=(TextView)findViewById(R.id.textView);
-        Dexter.checkPermission(new CompositePermissionListener(), Manifest.permission.READ_SMS);
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_SMS)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {/* ... */}
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {/* ... */}
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {/* ... */}
+                }).check();
     }
 
     @Override
